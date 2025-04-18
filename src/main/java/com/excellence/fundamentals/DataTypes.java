c6b6cbec35075732f1eacc4d46cdebc894fb6711
excellence <chintanpatel2013@icloud.com>
2025-04-18 20:08:00 +0530

    Add DataTypes class to demonstrate basic data types
    This class showcases the usage of Java's primitive data types with examples. It includes a main method that prints values of byte, short, int, long, float, double, boolean, and char. This provides a foundation for understanding fundamental data types in Java.

---
+++ src/main/java/com/excellence/fundamentals/DataTypes.java
@@ -0,0 +1,24 @@
+package com.excellence.fundamentals;
+
+public class DataTypes {
+    public static void main(String[] args) {
+
+        byte myByte = 10;
+        short myShort = 100;
+        int myInt = 1000;
+        long myLong = 1000000000L;
+        float myFloat = 10.10f;
+        double myDouble = 10.10;
+        boolean myBoolean = true;
+        char myChar = 'A';
+
+        System.out.println("Byte : " +  myByte);
+        System.out.println("Short : " + myShort);
+        System.out.println("Integer : " +  myInt);
+        System.out.println("Long : " + myLong);
+        System.out.println("Float : " +  myFloat);
+        System.out.println("Double : " +  myDouble);
+        System.out.println("Boolean : " +  myBoolean);
+        System.out.println("Character : " +  myChar);
+    }
+}
