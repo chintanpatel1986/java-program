8640cc7db790279a06ec19be7f1c008801431445
excellence <chintanpatel2013@icloud.com>
2025-04-18 19:40:21 +0530

    Add initial Java program and project dictionary config
    Introduce a basic Java program `SimpleJava` with simple print statements demonstrating Java's ease and power. Also, configure the project dictionary to include the word "chintan."

---
+++ src/main/java/com/excellence/SimpleJava.java
@@ -0,0 +1,12 @@
+package com.excellence;
+
+public class SimpleJava {
+    public static void main(String[] args) {
+        System.out.println("Hello, Chintan !");
+        System.out.println("Welcome to world of Java.");
+        System.out.println("Let's learn Java.");
+        System.out.println("Java is a powerful programming language.");
+        System.out.println("Java is easy to learn.");
+        System.out.println("Java is better than C++.");
+    }
+}
