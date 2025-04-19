9d711a74705e57f244a46db2859298249974bb34
excellence <chintanpatel2013@icloud.com>
2025-04-19 18:20:49 +0530

    Add an ArithmeticOperator class with basic operations
    Introduced the `ArithmeticOperator` class to demonstrate simple arithmetic operations (addition, subtraction, multiplication, division, and modulus). This provides a foundation for understanding basic operator functionality in Java.

---
+++ src/main/java/com/excellence/fundamentals/operator/ArithmeticOperator.java
@@ -0,0 +1,15 @@
+package com.excellence.fundamentals.operator;
+
+public class ArithmeticOperator {
+    public static void main(String[] args) {
+
+        int numberOne = 100;
+        int numberTwo = 60;
+
+        System.out.println(numberOne + numberTwo);
+        System.out.println(numberOne - numberTwo);
+        System.out.println(numberOne * numberTwo);
+        System.out.println(numberOne / numberTwo);
+        System.out.println(numberOne % numberTwo);
+    }
+}
