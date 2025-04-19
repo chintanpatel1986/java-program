72bb6ffe54e2e3e6b7d961cfb4c6e9d923170349
excellence <chintanpatel2013@icloud.com>
2025-04-19 18:37:01 +0530

    Add RelationalOperator class and inspection profile config
    Introduce a `RelationalOperator` class to demonstrate relational operators with sample outputs. Additionally, include the default inspection profile configuration for the project under `.idea` to manage code analysis settings.

---
+++ src/main/java/com/excellence/fundamentals/operator/RelationalOperator.java
@@ -0,0 +1,16 @@
+package com.excellence.fundamentals.operator;
+
+public class RelationalOperator {
+    public static void main(String[] args) {
+
+        int numberOne = 10;
+        int numberTwo = 60;
+
+        System.out.println("(numberOne < numberTwo)  : " + (numberOne < numberTwo));
+        System.out.println("(numberOne > numberTwo)  : " + (numberOne > numberTwo));
+        System.out.println("(numberOne <= numberTwo) : " + (numberOne <= numberTwo));
+        System.out.println("(numberOne >= numberTwo) : " + (numberOne >= numberTwo));
+        System.out.println("(numberOne == numberTwo) : " + (numberOne == numberTwo));
+        System.out.println("(numberOne != numberTwo) : " + (numberOne != numberTwo));
+    }
+}
